84aacb97f5eeb554a007469333591d152a274488
Nitish Gupta <nitesh4146@gmail.com>
2018-04-06 20:15:43 -0400

    .

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -9,12 +9,10 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
-
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-
 // Create the email and send the message
 $to = 'nitesh4146@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
