ce0cbd7ed3ca2e74b9cdfa33b592e420f0c1930a
Nitish Gupta <nitesh4146@gmail.com>
2020-06-28 16:14:59 +0530

    database functionality fixed

--- js/feed_timings.js
+++ js/feed_timings.js
@@ -15,12 +15,31 @@ var key = d.getDate().toString() + d.getMonth().toString();
 
 database.ref('/feed_table/' + key).set({
     meal1_feed1: false,
+    meal1_feed2: false,
+    meal1_feed3: false,
+    meal2_feed1: false,
+    meal2_feed2: false,
+    meal2_feed3: false,
+    meal3_feed1: false,
+    meal3_feed2: false,
+    meal3_feed3: false,
 });
+
 
 function myFunction() {
 
+    var all_meals_points = new Array(3); 
+    for (var i = 0; i < 3; i++) { 
+       all_meals_points[i] = []; 
+    }
+
+    for (var i = 0; i < 3; i++) { 
+        for (var j = 0; j < 3; j++) { 
+            all_meals_points[i][j] = false; 
+        } 
+    } 
+
     var text = document.getElementById("text");
-    var all_meals_points = [];
     document.getElementById("text").innerHTML = key;
     
     for (var meal = 1; meal <= 3; meal++) {
@@ -29,10 +48,20 @@ function myFunction() {
             if (checkBox.checked == true){
                 text.style.display = "block";
                 checkBox.disabled = true;
-                // "meal" + meal.toString() + "_feed" + feed_point.toString(): true;
-                
+                all_meals_points[meal-1][feed_point-1] = true;
             }
         }
     }
-    
+    // console.log(all_meals_points);
+    database.ref('/feed_table/' + key).set({
+        meal1_feed1: all_meals_points[0][0],
+        meal1_feed2: all_meals_points[0][1],
+        meal1_feed3: all_meals_points[0][2],
+        meal2_feed1: all_meals_points[1][0],
+        meal2_feed2: all_meals_points[1][1],
+        meal2_feed3: all_meals_points[1][2],
+        meal3_feed1: all_meals_points[2][0],
+        meal3_feed2: all_meals_points[2][1],
+        meal3_feed3: all_meals_points[2][2],
+    });
 }
